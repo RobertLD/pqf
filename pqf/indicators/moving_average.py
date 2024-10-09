96fefdc2f26728ca2f7220bf4ca87e34bf875b4e
Robertld <robert.derienzo@knights.ucf.edu>
2024-10-09 11:28:38 +0000

    Update test pipeline and add ruff formatting

--- pqf/indicators/moving_average.py
+++ pqf/indicators/moving_average.py
@@ -5,8 +5,12 @@ import polars as pl
 @overload
 def simple_moving_average(prices: pl.Series, window_size: int) -> pl.Series: ...
 
+
 @overload
 def simple_moving_average(prices: pl.Expr, window_size: int) -> pl.Expr: ...
 
-def simple_moving_average(prices: pl.Expr | pl.Series, window_size: int) -> pl.Expr | pl.Series:
-  return prices.rolling_mean(window_size)
+
+def simple_moving_average(
+    prices: pl.Expr | pl.Series, window_size: int
+) -> pl.Expr | pl.Series:
+    return prices.rolling_mean(window_size)
