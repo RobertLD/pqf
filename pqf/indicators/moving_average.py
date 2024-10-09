f42b590b4097dd30e79efb47cbcde757c428ec87
Robertld <robert.derienzo@knights.ucf.edu>
2024-10-09 12:46:55 +0000

    Finish exp moving average

--- pqf/indicators/moving_average.py
+++ pqf/indicators/moving_average.py
@@ -1,16 +1,40 @@
-from typing import overload
+from typing import Any
 import polars as pl
-
-
-@overload
-def simple_moving_average(prices: pl.Series, window_size: int) -> pl.Series: ...
-
-
-@overload
-def simple_moving_average(prices: pl.Expr, window_size: int) -> pl.Expr: ...
+import numpy as np
+from numba import guvectorize
 
 
 def simple_moving_average(
     prices: pl.Expr | pl.Series, window_size: int
 ) -> pl.Expr | pl.Series:
     return prices.rolling_mean(window_size)
+
+
+@guvectorize(["void(float64[:], float64, float64[:])"], "(n),(),(n)", target="cpu")
+def _vec_ewma(data, alpha, ema) -> Any:
+    n = data.shape[0]
+    if n == 0:
+        return
+    ema[0] = data[0]  # Set the first value of EMA to be the first data point
+
+    for i in range(1, n):
+        ema[i] = (data[i] * alpha) + (ema[i - 1] * (1 - alpha))
+
+
+def _numpy_ewma(data: np.ndarray, window_size: int) -> np.ndarray:
+    alpha = 2 / (window_size + 1.0)
+
+    # Prepare an array for the result
+    ema_result = np.empty_like(data)
+    _vec_ewma(data, alpha, ema_result)
+    return ema_result
+
+
+def exponential_moving_average(prices: pl.Series, window_size: int) -> pl.Series:
+    if isinstance(prices, pl.Series):
+        data = prices.to_numpy()
+        result = _numpy_ewma(data, window_size)
+        return pl.Series(result)
+
+    elif isinstance(prices, pl.Expr):
+        return prices.apply(lambda s: _vec_ewma(s.to_numpy()), return_dtype=pl.Float64)
--- tests/test_indicators/test_moving_averages.py
+++ tests/test_indicators/test_moving_averages.py
@@ -1,15 +1,29 @@
 import polars as pl
 import polars.testing as plt
-from pqf.indicators.moving_average import simple_moving_average
+from pqf.indicators.moving_average import (
+    simple_moving_average,
+    exponential_moving_average,
+)
 
 
-class TestSimpleMovingAverage:
-    def test_simple_moving_avarage_nan_padding(self):
-        prices = pl.Series([1, 2, 3, 4, 5])
-        result = simple_moving_average(prices, 3)
-        plt.assert_series_equal(result, pl.Series([None, None, 2.0, 3.0, 4.0]))
+class TestMovingAverage:
+    class TestSimpleMovingAverage:
+        def test_simple_moving_avarage_nan_padding(self):
+            prices = pl.Series([1, 2, 3, 4, 5])
+            result = simple_moving_average(prices, 3)
+            plt.assert_series_equal(result, pl.Series([None, None, 2.0, 3.0, 4.0]))
 
-    def test_simple_moving_avarage_nan_padding_with_window_size_as_array_length(self):
-        prices = pl.Series([1, 2, 3, 4, 5])
-        result = simple_moving_average(prices, 5)
-        plt.assert_series_equal(result, pl.Series([None, None, None, None, 3.0]))
+        def test_simple_moving_avarage_nan_padding_with_window_size_as_array_length(
+            self,
+        ):
+            prices = pl.Series([1, 2, 3, 4, 5])
+            result = simple_moving_average(prices, 5)
+            plt.assert_series_equal(result, pl.Series([None, None, None, None, 3.0]))
+
+    class TestExponentialMovingAverage:
+        def test_exp_moving_avarage_correctly_weights_vector(self):
+            prices = pl.Series([1, 2, 3, 4, 5], dtype=pl.Float64)
+            expected_result = pl.Series([1, 1.5, 2.25, 3.125, 4.0625], dtype=pl.Float64)
+
+            result = exponential_moving_average(prices, 3)
+            plt.assert_series_equal(result, expected_result)
