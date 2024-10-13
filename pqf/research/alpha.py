37bbf2f4c27016c3293493235246f34c4f077eaf
RobertLD <rlderienzo@gmail.com>
2024-10-13 23:31:59 +0000

    Adjust factors to work with multiple assets and multiple factors

--- pqf/research/alpha.py
+++ pqf/research/alpha.py
@@ -1,27 +1,57 @@
+import itertools
+from typing import NamedTuple
+
 import polars as pl
 import polars.selectors as cs
 
 
-def simple_factor_exposure(factors: pl.LazyFrame) -> pl.LazyFrame:
+class FactorAssetPair(NamedTuple):
+    factor: str
+    asset: str
+
+
+def simple_factor_exposure(
+    factors: pl.LazyFrame, factor_names: list[str]
+) -> pl.LazyFrame:
     return factors.with_columns(
-        cs.float()
-        .qcut([0.25, 0.75], labels=["-1", "0", "1"])
-        .cast(pl.Int8)
-        .name.suffix("_exposure")
+        *[
+            pl.col(factor)
+            .qcut([0.25, 0.75], labels=["-1", "0", "1"])
+            .cast(pl.Int8)
+            .name.keep()
+            for factor in factor_names
+        ]
     )
 
 
+def get_factor_asset_permutations(
+    factors: list[str], assets: list[str]
+) -> list[FactorAssetPair]:
+    return [
+        FactorAssetPair(factor, asset)
+        for factor, asset in itertools.product(factors, assets)
+    ]
+
+
 def simple_factor_returns(
-    factors: pl.LazyFrame, returns: pl.LazyFrame, date_column: str
+    factors: pl.LazyFrame,
+    returns: pl.LazyFrame,
+    date_column: str,
 ) -> pl.LazyFrame:
-    factor_exposure = simple_factor_exposure(factors)
+    factor_columns = factors.select(cs.float()).columns
+
+    factor_exposure = simple_factor_exposure(factors, factor_columns)
     factor_rank_df = returns.join(factor_exposure, on=date_column)
 
+    asset_columns = returns.select(cs.float()).columns
+    factor_returns_expressions = [
+        pl.col(fs.factor)
+        .mul(pl.col(fs.asset).shift(-1))
+        .alias(f"{fs.asset}_{fs.factor}_return")
+        for fs in get_factor_asset_permutations(factor_columns, asset_columns)
+    ]
     factor_return_df = factor_rank_df.select(
-        pl.col(date_column),
-        cs.contains("_exposure")
-        .mul(pl.col("returns").shift(-1))
-        .name.map(lambda col: col.replace("exposure", "return")),
+        pl.col(date_column), *factor_returns_expressions
     )
 
     return factor_return_df
--- tests/test_research/test_alpha.py
+++ tests/test_research/test_alpha.py
@@ -36,11 +36,66 @@ class TestFactorReturns:
                 ],
                 [0.01, 0.01, -0.01, 0.05, -0.07, 0.032],
             ],
-            schema=["timestamp", "returns"],
+            schema=["timestamp", "BTC"],
         ).lazy()
         factor_return_df = simple_factor_returns(factor, returns, "timestamp")
-        factor_returns = factor_return_df.collect().select("factor_return").to_series()
+        factor_returns = (
+            factor_return_df.collect().select("BTC_factor_return").to_series()
+        )
         plt.assert_series_equal(
             factor_returns,
-            pl.Series("factor_return", [0.0, 0.01, 0.05, 0.07, None]),
+            pl.Series("BTC_factor_return", [0.0, 0.01, 0.05, 0.07, None]),
         )
+
+    def test_simple_factor_returns_calculated_correctly_with_two_assets_and_one_factor(
+        self,
+    ):
+        factor = pl.DataFrame(
+            [
+                [
+                    datetime(2021, 1, 1),
+                    datetime(2021, 1, 2),
+                    datetime(2021, 1, 3),
+                    datetime(2021, 1, 4),
+                    datetime(2021, 1, 5),
+                    datetime(2021, 1, 6),
+                ],
+                [0.1, 0.1, -0.1, 0.5, -0.8, 0.3],
+                [0.1, 0.1, -0.1, 0.5, -0.8, 0.3],
+            ],
+            schema=["timestamp", "A", "B"],
+        ).lazy()
+        returns = pl.DataFrame(
+            [
+                [
+                    datetime(2021, 1, 2),
+                    datetime(2021, 1, 3),
+                    datetime(2021, 1, 4),
+                    datetime(2021, 1, 5),
+                    datetime(2021, 1, 6),
+                    datetime(2021, 1, 7),
+                ],
+                [0.01, 0.01, -0.01, 0.05, -0.07, 0.032],
+                [0.01, 0.01, -0.01, 0.05, -0.07, 0.032],
+            ],
+            schema=["timestamp", "BTC", "ETH"],
+        ).lazy()
+        factor_return_df = simple_factor_returns(factor, returns, "timestamp")
+        factor_returns = factor_return_df.collect()
+
+        expected_returns = pl.DataFrame(
+            {
+                "timestamp": [
+                    datetime(2021, 1, 2),
+                    datetime(2021, 1, 3),
+                    datetime(2021, 1, 4),
+                    datetime(2021, 1, 5),
+                    datetime(2021, 1, 6),
+                ],
+                "BTC_A_return": [0.0, 0.01, 0.05, 0.07, None],
+                "ETH_A_return": [0.0, 0.01, 0.05, 0.07, None],
+                "BTC_B_return": [0.0, 0.01, 0.05, 0.07, None],
+                "ETH_B_return": [0.0, 0.01, 0.05, 0.07, None],
+            }
+        )
+        plt.assert_frame_equal(factor_returns, expected_returns)
