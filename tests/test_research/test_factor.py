9baf2d883942e55900f2117782d7e94e003fbe88
Robert DeRienzo <rlderienzo@gmail.com>
2024-10-14 17:24:20 -0400

    Add support for cumulative simple returns (#2)

--- tests/test_research/test_factor.py
+++ tests/test_research/test_factor.py
@@ -99,3 +99,45 @@ class TestFactorReturns:
             }
         )
         plt.assert_frame_equal(factor_returns, expected_returns)
+
+    def test_cumulative_simple_factor_returns_calculated_correctly_with_single_asset_and_factor(
+        self,
+    ):
+        factor = pl.DataFrame(
+            [
+                [
+                    datetime(2021, 1, 1),
+                    datetime(2021, 1, 2),
+                    datetime(2021, 1, 3),
+                    datetime(2021, 1, 4),
+                    datetime(2021, 1, 5),
+                    datetime(2021, 1, 6),
+                ],
+                [0.1, 0.1, -0.1, 0.5, -0.8, 0.3],
+            ],
+            schema=["timestamp", "factor"],
+        ).lazy()
+        returns = pl.DataFrame(
+            [
+                [
+                    datetime(2021, 1, 2),
+                    datetime(2021, 1, 3),
+                    datetime(2021, 1, 4),
+                    datetime(2021, 1, 5),
+                    datetime(2021, 1, 6),
+                    datetime(2021, 1, 7),
+                ],
+                [0.01, 0.01, -0.01, 0.05, -0.07, 0.032],
+            ],
+            schema=["timestamp", "BTC"],
+        ).lazy()
+        factor_return_df = simple_factor_returns(
+            factor, returns, "timestamp", cumulative=True
+        )
+        factor_returns = (
+            factor_return_df.collect().select("BTC_factor_return").to_series()
+        )
+        plt.assert_series_equal(
+            factor_returns,
+            pl.Series("BTC_factor_return", [0.0, 0.01, 0.06, 0.13, None]),
+        )
