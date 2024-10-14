bef537a3348065ab18d1e475f5d2356f58ca57b7
Robert DeRienzo <rlderienzo@gmail.com>
2024-10-13 20:58:56 -0400

    Add simple sharpe-ratio calculation (#3)
    * Add simple sharpe-ratio calculation
    
    * Resolve typing issues in stub file

---
+++ typings/pqf/research/statistics.pyi
@@ -0,0 +1,9 @@
+from typing import TYPE_CHECKING, overload
+
+if TYPE_CHECKING:
+    import polars as pl
+
+@overload
+def sharpe_ratio(returns: pl.Series, risk_free_rate: float) -> float | None: ...
+@overload
+def sharpe_ratio(returns: pl.Expr, risk_free_rate: float) -> pl.Expr: ...
