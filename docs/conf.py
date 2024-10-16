8f316c78147cedbea6a68086e81546de15876e46
Robertld <robert.derienzo@knights.ucf.edu>
2024-10-16 01:05:27 +0000

    Final sphinx fixes

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,10 @@
 #
 import sys
 import os
+import pkgutil
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
+sys.path.insert(0, os.path.abspath("../.."))
 project = "PQF"
 copyright = "2024, RobertLD"
 author = "RobertLD"
@@ -34,7 +35,6 @@ html_use_modindex = True
 html_static_path = ["_static"]
 html_theme_options = {
     "prev_next_buttons_location": "bottom",
-    "display_version": True,
     "logo_only": False,
     # Toc options
     "collapse_navigation": False,
@@ -49,4 +49,30 @@ autosummary_generate = True
 autodoc_default_options = {
     "inherited-members": None,
 }
-autodoc_typehints = "none"
+autodoc_typehints = "both"
+
+# generate_rst.py
+
+
+def generate_autosummary_rst(package_name):
+    submodules = list_submodules(package_name)
+    with open("index.rst", "w") as f:
+        f.write(f"{package_name} Documentation\n")
+        f.write("=================\n\n")
+        f.write(".. rubric:: Modules\n\n")
+        f.write(".. autosummary::\n")
+        f.write("    :recursive:\n")
+        f.write("    :toctree: generated\n\n")
+        for module in submodules:
+            f.write(f"    {module}\n")
+
+
+def list_submodules(package_name):
+    package = __import__(package_name, fromlist=[""])
+    return [
+        name
+        for _, name, _ in pkgutil.iter_modules(package.__path__, package_name + ".")
+    ]
+
+
+generate_autosummary_rst("pqf")
